7c4a49ecf2f3db4f80c8cee1cd7ace2bd309d54e
Robin <robinlandais75@yahoo.com>
2024-06-27 15:17:25 +0200

    Init projet

--- research/models.py
+++ research/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Chercheur(models.Model):
+    nom = models.CharField(max_length=100)
+    specialite = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
+
+class ProjetDeRecherche(models.Model):
+    titre = models.CharField(max_length=200)
+    description = models.TextField()
+    date_debut = models.DateField()
+    date_fin_prevue = models.DateField()
+    chef_projet = models.ForeignKey(Chercheur, on_delete=models.CASCADE, related_name='projets')
+
+    def __str__(self):
+        return self.titre
+
+class Publication(models.Model):
+    titre = models.CharField(max_length=200)
+    resume = models.TextField()
+    projet_associe = models.ForeignKey(ProjetDeRecherche, on_delete=models.CASCADE, related_name='publications')
+    date_publication = models.DateField()
+
+    def __str__(self):
+        return self.titre
--- research/views.py
+++ research/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Chercheur, ProjetDeRecherche, Publication
+from .serializers import ChercheurSerializer, ProjetDeRechercheSerializer, PublicationSerializer
 
-# Create your views here.
+class ChercheurViewSet(viewsets.ModelViewSet):
+    queryset = Chercheur.objects.all()
+    serializer_class = ChercheurSerializer
+
+class ProjetDeRechercheViewSet(viewsets.ModelViewSet):
+    queryset = ProjetDeRecherche.objects.all()
+    serializer_class = ProjetDeRechercheSerializer
+
+class PublicationViewSet(viewsets.ModelViewSet):
+    queryset = Publication.objects.all()
+    serializer_class = PublicationSerializer
